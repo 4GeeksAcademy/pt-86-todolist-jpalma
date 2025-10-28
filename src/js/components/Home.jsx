c6a4b7c7868555413c6d3351ea2f5ed33aa2f9ed
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-10-28 19:47:02 +0000

    todo list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,21 +5,54 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-const [todoList, setTodoList] =React.useState("")
+const [tasks, setTasks] =React.useState(["Run 3 miles", "Shower", "Study"])
+const[userInput, setUserInput]=React.useState("")
 
+const newTask=()=>{
+	if(userInput ===""){
+		return alert("You must type something in! (e.g. Eat BreakFast)")
+	}else{
+		setTasks([...tasks, userInput])
+		setUserInput("")
+	}
+
+
+}
+const enterKey=(event)=>{
+	if (event.key === 'Enter'){
+		setTasks([...tasks, userInput])
+		setUserInput("")
+		
+	 event.preventDefault(); 
+	}
+}
+
+const Remove=(index)=>{
+	setTasks(tasks.filter((task,i) => i!== index))
+}
+const defaultChecked=()=>{
+
+}
 
 	return (
-		<div className="container text-center">
-			<h2>TO-DO List</h2>
-			<input type="text" placeholder="Enter something here ..."
-			 onChange={(e) => setTodoList(e.target.value)}
-			 value={todoList}/>
-			<button className="add">Add to your list</button>
+		<div className="container">
+			<h3>TO-DO List</h3>
+			<div input-container>
+				<input className="text-input" type="text" placeholder="Enter task here ..."
+				onChange={(e) => setUserInput(e.target.value)}
+				value={userInput}
+				onKeyDown={enterKey}/>
+				<button className="add" onClick={newTask}>Add Task</button>
+			</div>
 			<div>
 				<ul className="lists">
-					<li>{item}</li>
-					<li>Study</li>
-					<li>Class</li>
+					{tasks.map((task, index) =>
+						<li key={index}>
+							<input className="checked" type="checkbox" defaultChecked={defaultChecked}/>
+							<label>{task}</label>
+							<button className="remove" onClick={()=>Remove(index)}>🗑️</button>	
+						</li>
+					)}
 				</ul>
 			</div>
 		</div>
